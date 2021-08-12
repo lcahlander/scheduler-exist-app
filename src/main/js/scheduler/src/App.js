751d23e79b3cfda5f4eb65d20fd282fab5bbc6c8
lcahlander <loren.cahlander@gmail.com>
2021-08-12 13:58:46 -0400

    Setting initial React.js app UI layout

--- src/main/js/scheduler/src/App.js
+++ src/main/js/scheduler/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { HashRouter as Router } from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    render() {
+        return (
+            <Router>
+                <Navbar bg="dark" variant="dark" fixed="top">
+                    <Navbar.Brand href="./">
+                        <img
+                            id="logo"
+                            alt=""
+                            src="icon.png"
+                            height="40"
+                        />{' '}Scheduler</Navbar.Brand>
+                </Navbar>
+                <Container style={{marginTop: "70px"}} fluid>
+                    <Row xs={1}>
+                        <Col md={4} xl={3} xs={12} class="sidenav" >
+                        </Col>
+                        <Col md={8} xl={7} xs={12}>
+                        </Col>
+                    </Row>
+                </Container>
+            </Router>
+        )
+    }
+
 }
 
 export default App;
